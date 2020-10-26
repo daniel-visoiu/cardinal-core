71d3b61461eb4cb1859946248c5408897d9e300d
Daniel Visoiu <daniel.visoiu@toptal.com>
2020-10-26 21:22:26 +0200

    Added utils to window.cardinalCore.

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import * as controllers from "./controllers";
 import * as decorators from "./decorators";
 import * as events from "./events";
 import * as services from "./services";
+import * as utils from "./utils";
 
 export * from "./controllers";
 export * from "./decorators";
 export * from "./events";
 export * from "./services";
+export * from "./utils";
 
 const cardinalCore = {
   ...controllers,
   ...decorators,
   ...events,
   ...services,
+  ...utils,
 };
 
 type CardinalCore = typeof cardinalCore;
